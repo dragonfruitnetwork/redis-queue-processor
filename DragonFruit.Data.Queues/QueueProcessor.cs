f365226b2d7ad55f59a4397edc59df785c2b3103
Albie <albion@dragonfruit.network>
2022-09-04 12:53:09 +0100

    add queue processing mechanisms

---
+++ DragonFruit.Data.Queues/QueueProcessor.cs
@@ -0,0 +1,191 @@
+﻿// Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under MIT. Refer to the LICENSE file for more info
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using DragonFruit.Data.Queues.Jobs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Nito.AsyncEx;
+using StackExchange.Redis;
+
+namespace DragonFruit.Data.Queues
+{
+    /// <summary>
+    /// A lightweight job processor using redis as an intermediate storage provider.
+    /// </summary>
+    public class QueueProcessor<T> : BackgroundService where T : Job
+    {
+        private int _maxConcurrency;
+        private readonly string _queueKey;
+        private readonly string _queueEventsKey;
+
+        private readonly ILogger _logger;
+        private readonly IConnectionMultiplexer _redis;
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        private readonly AsyncManualResetEvent _processorSignal = new();
+        private readonly IDictionary<string, Type> _jobMap = new Dictionary<string, Type>();
+
+        public QueueProcessor(ILogger logger, IServiceScopeFactory scopeFactory, IConnectionMultiplexer redis, string queueKey, int databaseId = 0)
+        {
+            _redis = redis;
+            _logger = logger;
+            _scopeFactory = scopeFactory;
+
+            _queueKey = queueKey;
+            _queueEventsKey = $"__keyspace@{databaseId}__:{queueKey}";
+
+            if (typeof(T) == typeof(Job))
+            {
+                // register tasks by assembly
+                RegisterJobs(Assembly.GetEntryAssembly());
+            }
+            else
+            {
+                // register tasks by assembly
+                var type = typeof(T);
+                _jobMap[GetJobTypeId(type)] = type;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the max batch size the processor can consume at one time.
+        /// Setting this to 0 will prevent it from retrieving queued jobs.
+        /// </summary>
+        public int MaxConcurrency
+        {
+            get => _maxConcurrency;
+            set => _maxConcurrency = Math.Max(value, 0);
+        }
+
+        /// <summary>
+        /// The <see cref="JsonSerializerOptions"/> to use when serializing/deserializing queue entries
+        /// </summary>
+        public JsonSerializerOptions SerializerOptions { get; set; }
+
+        /// <summary>
+        /// Register jobs to be runnable on this queue
+        /// </summary>
+        /// <param name="assembly">The assembly to reflect to discover job types</param>
+        /// <exception cref="DuplicateNameException">The job has already been registered, or a duplicate name has been found</exception>
+        public void RegisterJobs(Assembly assembly)
+        {
+            foreach (var type in assembly.ExportedTypes.Where(x => !x.IsAbstract && !x.IsInterface && typeof(Job).IsAssignableFrom(x)))
+            {
+                var typeId = GetJobTypeId(type);
+
+                // ensure there are no duplicates
+                if (_jobMap.ContainsKey(typeId))
+                {
+                    throw new DuplicateNameException($"Duplicate key {typeId} was found");
+                }
+
+                _jobMap[typeId] = type;
+            }
+        }
+
+        /// <summary>
+        /// Queue a collection of jobs to be run on the task processor
+        /// </summary>
+        /// <param name="jobs"></param>
+        public async Task Enqueue(params T[] jobs)
+        {
+            var index = 0;
+            var convertedJobs = new SortedSetEntry[jobs.Length];
+
+            foreach (var job in jobs)
+            {
+                // needs to be an object to get serialization to work
+                var jobEntry = new JobWrapper<object>(GetJobTypeId(job.GetType()), job);
+                var utf8Bytes = JsonSerializer.SerializeToUtf8Bytes<object>(jobEntry, SerializerOptions);
+
+                // use unix epoch as the score
+                convertedJobs[index++] = new SortedSetEntry(utf8Bytes.AsMemory(), DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+            }
+
+            await _redis.GetDatabase().SortedSetAddAsync(_queueKey, convertedJobs, SortedSetWhen.NotExists).ConfigureAwait(false);
+            _processorSignal.Set();
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken cancellation)
+        {
+            await _redis.GetSubscriber().SubscribeAsync(_queueEventsKey, OnQueueEvent).ConfigureAwait(false);
+
+            while (true)
+            {
+                // wait for processing
+                await _processorSignal.WaitAsync(cancellation).ConfigureAwait(false);
+
+                // return if we were cancelled
+                if (cancellation.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                if (MaxConcurrency == 0)
+                {
+                    _processorSignal.Reset();
+                    continue;
+                }
+
+                _logger.Log(LogLevel.Information, "Queue processing started");
+                var jobBatch = Array.Empty<SortedSetEntry>();
+
+                do
+                {
+                    try
+                    {
+                        // get job and process each one
+                        _logger.Log(LogLevel.Debug, "Fetching next batch of items");
+                        jobBatch = await _redis.GetDatabase().SortedSetPopAsync(_queueKey, MaxConcurrency).ConfigureAwait(false);
+
+                        if (!jobBatch.Any())
+                        {
+                            continue;
+                        }
+
+                        // convert batch to tasks
+                        var jobs = jobBatch.Select(x =>
+                        {
+                            var job = JsonSerializer.Deserialize<T>((byte[])x.Element, SerializerOptions);
+
+                            using var scope = _scopeFactory.CreateScope();
+                            return job?.Perform(scope) ?? Task.CompletedTask;
+                        });
+
+                        // wait for completion
+                        await Task.WhenAll(jobs).ConfigureAwait(false);
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.Log(LogLevel.Error, e, "Queue processing failed ({queue})", _queueKey);
+                    }
+                } while (jobBatch.Any() && !cancellation.IsCancellationRequested);
+
+                // reset processing signal
+                _processorSignal.Reset();
+            }
+
+            await _redis.GetSubscriber().UnsubscribeAsync(_queueEventsKey, OnQueueEvent).ConfigureAwait(false);
+        }
+
+        private void OnQueueEvent(RedisChannel channel, RedisValue action)
+        {
+            if (action.ToString().Equals("zadd", StringComparison.OrdinalIgnoreCase))
+            {
+                // get the queue processor started
+                _processorSignal.Set();
+            }
+        }
+
+        private static string GetJobTypeId(MemberInfo type) => type.GetCustomAttribute<JobTypeId>()?.TypeId ?? type.Name;
+    }
+}
---
+++ DragonFruit.Data.Queues/QueueProcessorExtensions.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under MIT. Refer to the LICENSE file for more info
+
+using DragonFruit.Data.Queues.Jobs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+namespace DragonFruit.Data.Queues
+{
+    public static class QueueProcessorExtensions
+    {
+        /// <summary>
+        /// Registers a generic queue processor
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+        /// <param name="queueKey">The key of the redis object</param>
+        /// <param name="databaseId">The id of the database to listen for changes on. Defaults to database 0</param>
+        public static void AddQueueProcessor(this IServiceCollection services, string queueKey, int databaseId = 0)
+        {
+            AddQueueProcessor<Job>(services, queueKey, databaseId);
+        }
+
+        /// <summary>
+        /// Registers a dedicated type queue processor
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+        /// <param name="queueKey">The key of the redis object</param>
+        /// <param name="databaseId">The id of the database to listen for changes on. Defaults to database 0</param>
+        public static void AddQueueProcessor<T>(this IServiceCollection services, string queueKey, int databaseId = 0) where T : Job
+        {
+            services.AddSingleton(s =>
+            {
+                var logger = s.GetRequiredService<ILogger<QueueProcessor<T>>>();
+                var redis = s.GetRequiredService<IConnectionMultiplexer>();
+                var scope = s.GetRequiredService<IServiceScopeFactory>();
+
+                return new QueueProcessor<T>(logger, scope, redis, queueKey, databaseId);
+            });
+
+            services.AddHostedService(s => s.GetRequiredService<QueueProcessor<T>>());
+        }
+    }
+}
