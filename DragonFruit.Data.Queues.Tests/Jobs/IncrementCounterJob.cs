37f5f39c2636d93767efe2cadc3fccc2cade6c9c
Albie <albion@dragonfruit.network>
2022-09-05 09:55:06 +0100

    add test jobs

---
+++ DragonFruit.Data.Queues.Tests/Jobs/IncrementCounterJob.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under MIT. Refer to the LICENSE file for more info
+
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using DragonFruit.Data.Queues.Jobs;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace DragonFruit.Data.Queues.Tests.Jobs
+{
+    public class IncrementCounterJob : Job
+    {
+        internal const string RedisKey = "test-counter";
+
+        public override bool AllowDuplicates => true;
+
+        public IncrementCounterJob(int value = 1)
+        {
+            Value = value;
+        }
+
+        [JsonPropertyName("value")]
+        public int Value { get; set; }
+
+        public override Task Perform(IServiceScope scope)
+        {
+            var redis = scope.ServiceProvider.GetRequiredService<IConnectionMultiplexer>();
+            return redis.GetDatabase().StringIncrementAsync(RedisKey, Value);
+        }
+    }
+}
---
+++ DragonFruit.Data.Queues.Tests/Jobs/SetSwitchJob.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under MIT. Refer to the LICENSE file for more info
+
+using System.Threading.Tasks;
+using DragonFruit.Data.Queues.Jobs;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace DragonFruit.Data.Queues.Tests.Jobs
+{
+    public class SetSwitchJob : Job
+    {
+        internal const string RedisKey = "test-switch";
+
+        public override Task Perform(IServiceScope scope)
+        {
+            var redis = scope.ServiceProvider.GetRequiredService<IConnectionMultiplexer>();
+            return redis.GetDatabase().StringSetAsync(RedisKey, "1");
+        }
+    }
+}
