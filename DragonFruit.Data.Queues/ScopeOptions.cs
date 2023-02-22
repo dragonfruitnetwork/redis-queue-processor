a12242622895ef9034d2a66fd0fee39d9109ed71
Albie <albion@dragonfruit.network>
2023-02-22 08:53:46 +0000

    add scope options enum

---
+++ DragonFruit.Data.Queues/ScopeOptions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) DragonFruit Network <inbox@dragonfruit.network>
+// Licensed under MIT. Refer to the LICENSE file for more info
+
+using System;
+
+namespace DragonFruit.Data.Queues
+{
+    public enum ScopeOptions
+    {
+        /// <summary>
+        /// A new <see cref="IServiceProvider"/> will be created for each job
+        /// </summary>
+        PerJob,
+
+        /// <summary>
+        /// A new <see cref="IServiceProvider"/> will be created for each batch
+        /// </summary>
+        PerBatch,
+
+        /// <summary>
+        /// A new <see cref="IServiceProvider"/> will be created at the start, and reused until there are no jobs pending.
+        /// This is ideal for sharing resources but not recommended on high-volume pipelines.
+        /// </summary>
+        PerCycle
+    }
+}
